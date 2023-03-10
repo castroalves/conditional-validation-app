493be8c49286427e824980758ce33cabca61836a
Cadu de Castro Alves <cadudecastroalves@gmail.com>
2023-03-10 14:12:40 +0100

    Implement conditional validation example

--- pages/field.tsx
+++ pages/field.tsx
@@ -1,23 +1,51 @@
 import { useFieldExtension, Wrapper } from "@graphcms/app-sdk-react";
-import { Flex, Input } from "@hygraph/baukasten";
+import { InputError } from "@hygraph/baukasten";
+import { useEffect, useState } from "react";
 
-function FieldElement() {
-    const { value, onChange } = useFieldExtension();
-    return (
-        <Flex>
-            <Input
-                placeholder="Type something here"
-                value={value || ""}
-                onChange={(e: any) => onChange(e.target.value)}
-            />
-        </Flex>
-    );
+function GuardianField() {
+    const sdkProps = useFieldExtension();
+    const [hasAuthors, setHasAuthors] = useState(false);
+
+    useEffect(() => {
+        // we can set any value, as long as its invalid
+        // e.g the declaration says this is a string field, but, we set its value to a number to make it invalid as soon as its rendered
+        // later on we can choose to set a valid value
+        sdkProps.onChange(1);
+    }, []);
+
+    useEffect(() => {
+        let unsubscriber: any = undefined;
+        sdkProps.form
+            .subscribeToFormState(
+                ({ values }) => {
+                    // check if authors fields has any value
+                    if (values["authors"].connect.length > 0) {
+                        setHasAuthors(true);
+                        // if it does, set the value to a valid value (string)
+                        sdkProps.onChange("validate");
+                    } else {
+                        setHasAuthors(false);
+                        sdkProps.onChange(1);
+                    }
+                },
+                { values: true }
+            )
+            .then((unsub) => {
+                unsubscriber = unsub;
+            });
+
+        return () => {
+            unsubscriber?.();
+        };
+    }, []);
+
+    return hasAuthors ? <div>Valid</div> : <InputError>Invalid</InputError>;
 }
 
-export default function Field() {
+export default function App() {
     return (
         <Wrapper>
-            <FieldElement />
+            <GuardianField />
         </Wrapper>
     );
 }
